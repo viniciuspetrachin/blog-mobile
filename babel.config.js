d4047490e925fc15197f4fe03e9dc0fdb0f05510
Vinicius Petrachin <vinicius@inovaebiz.com.br>
2023-07-22 09:13:47 -0300

    chore: initial structure

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '~': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@hooks': './src/hooks',
+          },
+        },
+      ],
+    ],
   };
 };
